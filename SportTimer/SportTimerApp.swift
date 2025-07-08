ae28e9d3c9511a6a337fa9535e2086f887d45c33
Aver Alexey <68560245+cosmograve@users.noreply.github.com>
2025-07-08 17:32:41 +0700

    Initial commit

---
+++ SportTimer/SportTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SportTimerApp.swift
+//  SportTimer
+//
+//  Created by Алексей Авер on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct SportTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
